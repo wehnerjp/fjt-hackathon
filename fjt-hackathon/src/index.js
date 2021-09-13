15b2d4afbc85682f04cdaa5150f26894c1adf0e8
john.wehner <john.wehner@asurion.com>
2021-09-13 12:21:37 -0400

    flipcards

--- fjt-hackathon/src/index.js
+++ fjt-hackathon/src/index.js
@@ -1,17 +1,136 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Flipper, Flipped } from "react-flip-toolkit";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const listData = [...Array(7).keys()];
+const createCardFlipId = index => `listItem-${index}`;
+
+const shouldFlip = index => (prev, current) =>
+  index === prev || index === current;
+
+const ListItem = ({ index, onClick }) => {
+  return (
+    <Flipped
+      flipId={createCardFlipId(index)}
+      stagger="card"
+      shouldInvert={shouldFlip(index)}
+    >
+      <div className="listItem" onClick={() => onClick(index)}>
+        <Flipped inverseFlipId={createCardFlipId(index)}>
+          <div className="listItemContent">
+            <Flipped
+              flipId={`avatar-${index}`}
+              stagger="card-content"
+              shouldFlip={shouldFlip(index)}
+              delayUntil={createCardFlipId(index)}
+            >
+              <div className="avatar" />
+            </Flipped>
+            <div className="description">
+              {listData.slice(0, 3).map(i => (
+                <Flipped
+                  flipId={`description-${index}-${i}`}
+                  stagger="card-content"
+                  shouldFlip={shouldFlip(index)}
+                  delayUntil={createCardFlipId(index)}
+                >
+                  <div />
+                </Flipped>
+              ))}
+            </div>
+          </div>
+        </Flipped>
+      </div>
+    </Flipped>
+  );
+};
+
+const ExpandedListItem = ({ index, onClick }) => {
+  return (
+    <Flipped
+      flipId={createCardFlipId(index)}
+      stagger="card"
+      onStart={el => {
+        setTimeout(() => {
+          el.classList.add("animated-in");
+        }, 400);
+      }}
+    >
+      <div className="expandedListItem" onClick={() => onClick(index)}>
+        <Flipped inverseFlipId={createCardFlipId(index)}>
+          <div className="expandedListItemContent">
+            <Flipped
+              flipId={`avatar-${index}`}
+              stagger="card-content"
+              delayUntil={createCardFlipId(index)}
+            >
+              <div className="avatar avatarExpanded" />
+            </Flipped>
+            <div className="description">
+              {listData.slice(0, 3).map(i => (
+                <Flipped
+                  flipId={`description-${index}-${i}`}
+                  stagger="card-content"
+                  delayUntil={createCardFlipId(index)}
+                >
+                  <div />
+                </Flipped>
+              ))}
+            </div>
+            <div className="additional-content">
+              {listData.slice(0, 3).map(i => (
+                <div />
+              ))}
+            </div>
+          </div>
+        </Flipped>
+      </div>
+    </Flipped>
+  );
+};
+
+class AnimatedList extends Component {
+  state = { focused: null };
+  onClick = index =>
+    this.setState({
+      focused: this.state.focused === index ? null : index
+    });
+  render() {
+    return (
+      <Flipper
+        flipKey={this.state.focused}
+        className="staggered-list-content"
+        spring="gentle"
+        staggerConfig={{
+          card: {
+            reverse: this.state.focused !== null
+          }
+        }}
+        decisionData={this.state.focused}
+      >
+        <ul className="list">
+          {listData.map(index => {
+            return (
+              <li key={index}>
+                {index === this.state.focused ? (
+                  <ExpandedListItem
+                    index={this.state.focused}
+                    onClick={this.onClick}
+                  />
+                ) : (
+                  <ListItem index={index} key={index} onClick={this.onClick} />
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </Flipper>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<AnimatedList />, rootElement);
+
